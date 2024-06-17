9997cdf6626de530ddc8afa274466d5ac15fb569
Jophine07 <jophinepaul@gmail.com>
2024-06-17 09:43:04 +0530

    Started

--- models/Absent.js
+++ models/Absent.js
@@ -1,7 +1,19 @@
 const mongoose=require("mongoose")
 const schema=mongoose.Schema(
     {
-
+        "HOD_username":{type:String,required:true},
+        "HOD_Password":{type:String,required:true},
+        "student_username":{type:String,required:true},
+        "student_password":{type:String,required:true},
+        "faculty_username":{type:String,required:true},
+        "faculty_password":{type:String,required:true},
+        "Sname":{type:String,required:true},
+        "Fname":{type:String,required:true},
+        "Sid":{type:String,required:true},
+        "Fid":{type:String,required:true},
+        "F_reason":{type:String,required:true},
+        "S_reason":{type:String,required:true},
+        
     }
 )
 
