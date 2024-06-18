efb9b1ec69c2efcf515ebe6428d0a074ddd80b39
lenexwilson <lenexwilson@gmail.com>
2024-06-19 01:01:05 +0530

    not all

--- app.js
+++ app.js
@@ -4,3 +4,49 @@ const mongoose=require("mongoose")
 
 const{absentModel}=require("./models/Absent.js")
 
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://lenex:ducati@cluster0.kfyoghs.mongodb.net/absentdb?retryWrites=true&w=majority&appName=Cluster0")
+
+
+app.post("/AddLeave",(req,res)=>{
+    let input=req.body
+    let leave=new absentModel(input)
+    leave.save()
+    console.log(leave)
+    res.json({status:"test"})
+})
+
+app.post("/SearchLeave",(req,res)=>{
+  let input=req.body
+    absentModel.find(input).then((data)=>{
+        res.json(data)
+    })
+})
+
+app.post("/DeleteLeave",(req,res)=>{
+    let input=req.body
+    absentModel.findByIdAndDelete(input._id).then(
+        (response)=>{
+            res.json({status:"test"})
+
+        }
+)})
+
+app.get("/ViewAll",(req,res)=>{
+    absentModel.find().then(
+        (data)=>{
+            res.json(data)
+        }
+    ).catch(
+        (error)=>{
+            res.json(error)
+        })
+})
+
+app.listen(8005,()=>{
+    console.log("server started")
+})
+
